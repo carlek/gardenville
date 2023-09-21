56c17c04022f60118f513ae995d5a4d40afb1336
Carl Ek <carlek@m1.local>
2023-09-21 09:43:48 -0400

    add icrc1 ledger

---
+++ icrc1/icrc1-deploy.sh
@@ -0,0 +1,26 @@
+# Change the variable to "ic" to deploy the ledger on the mainnet.
+export NETWORK=local
+
+# Change the variable to the principal that can mint and burn tokens.
+export MINTER_PRINCIPAL=$(dfx identity get-principal)
+
+# Change the variable to the principal that controls archive canisters.
+export ARCHIVE_CONTROLLER=$(dfx identity get-principal)
+
+export TOKEN_NAME="GardenVilleToken"
+export TOKEN_SYMBOL=XGVT
+
+dfx deploy --network ${NETWORK} icrc1_ledger --argument '(variant { Init = 
+      record {
+        token_name = "'${TOKEN_NAME}'";
+        token_symbol = "'${TOKEN_SYMBOL}'";
+        minting_account = record { owner = principal "'${MINTER_PRINCIPAL}'";};
+        initial_balances = vec {};
+        metadata = vec {};
+        transfer_fee = 10;
+        archive_options = record {
+          trigger_threshold = 2000;
+          num_blocks_to_archive = 1000;
+          controller_id = principal "'${ARCHIVE_CONTROLLER}'";
+        }
+}})'
